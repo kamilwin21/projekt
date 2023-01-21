0773664676205df4f2cbf0fd102e27a9a291972c
kamilwin21 <65176862+kamilwin21@users.noreply.github.com>
2023-01-21 13:26:06 +0100

    implement doctor controller

--- src/main/java/pl/kul/projekt/doctor/DoctorController.java
+++ src/main/java/pl/kul/projekt/doctor/DoctorController.java
@@ -1,12 +1,31 @@
 package pl.kul.projekt.doctor;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctor")
 @RequiredArgsConstructor
 public class DoctorController {
+
     private final DoctorService doctorService;
+
+    @PostMapping
+    public Doctor add(@RequestBody Doctor doctor) {
+        return doctorService.add(doctor);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody Doctor doctor) {
+        doctorService.delete(doctor);
+    }
+
+    @GetMapping("/all")
+    public List<Doctor> getAllBySpecialization(@RequestParam("specialization") DoctorSpecialization specialization) {
+        return doctorService.getAllBySpecialization(specialization);
+    }
+
+
 }
--- src/main/java/pl/kul/projekt/doctor/DoctorService.java
+++ src/main/java/pl/kul/projekt/doctor/DoctorService.java
@@ -3,8 +3,23 @@ package pl.kul.projekt.doctor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class DoctorService {
+
     private final DoctorRepository doctorRepository;
+
+    public Doctor add(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    public void delete(Doctor doctor) {
+        doctorRepository.delete(doctor);
+    }
+
+    public List<Doctor> getAllBySpecialization(DoctorSpecialization specialization) {
+        return doctorRepository.getAllBySpecialization(specialization);
+    }
 }
